071ed9b9faea9d227451e54f3c9ecb22551f7cf0
Pyraquas <mathias.da.costa@outlook.com>
2026-03-08 01:08:54 +0100

    Implement first version of magic specific messages handling for Discord bot commands

---
+++ src/magic/message.rs
@@ -0,0 +1,30 @@
+use serenity::{all::User, model::channel::Message, prelude::Context};
+
+pub async fn handle_message(ctx: &Context, msg: &Message) -> Option<String> {
+    let command_args: Vec<&str> = msg.content.split_whitespace().collect();
+    if command_args.is_empty() || !command_args[0].starts_with('!') {
+        return None;
+    }
+    match command_args[0] {
+        "!help" => Some(String::from("Commande disponible dans ce salon: !help")),
+        "!win" => {
+            let answer;
+            if msg.mentions.len() == 1 && command_args.len() == 2 {
+                answer =
+                    String::from("Ajout d'une victoire pour ") + &msg.mentions[0].display_name();
+            } else {
+                answer = String::from("Usage: !win [@user]");
+            }
+            Some(answer)
+        }
+        "!leaderboard" => Some(String::from("Affichage du leaderboard")),
+
+        _ => Some(String::from(
+            "Commande inconnu ou indisponible dans ce salon",
+        )),
+    }
+}
+
+fn win(user: &User) -> String {
+    String::from("Ajout d'une victoire pour ") + &user.name.to_owned()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+mod magic;
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        match msg.channel_id.name(&ctx).await.unwrap().as_str() {
+            "bot-sandbox" => {
+                if let Some(response) = magic::message::handle_message(&ctx, &msg).await {
+                    if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
+                        println!("Error sending message: {:?}", why);
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Error starting client: {:?}", why);
+    }
 }
